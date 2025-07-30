67d3b484185421de21ae3a44fd44601f5460decd
Bello Abdullahi <6ty3ee@gmail.com>
2025-07-30 16:57:09 +0100

    Add complete NestJS API with auth, mesh-nodes, and users modules

---
+++ meshnode-api/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MeshNodesModule } from './mesh-nodes/mesh-nodes.module';
+import { getDatabaseConfig } from './database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getDatabaseConfig,
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    MeshNodesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
