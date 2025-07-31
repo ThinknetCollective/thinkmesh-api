46b922d116b250df4da83383f8faa8c408ea24b2
Musa Khalid <112591148+Mkalbani@users.noreply.github.com>
2025-07-31 15:13:43 +0000

    feat: implement tag suggestion and filtering system with OpenAI integration

--- src/mesh-nodes/mesh-nodes.module.ts
+++ src/mesh-nodes/mesh-nodes.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MeshNodesService } from './mesh-nodes.service';
+import { MeshNodesController } from './mesh-nodes.controller';
+import { MeshNode } from './entities/mesh-node.entity';
+import { TagsModule } from '../tags/tags.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([MeshNode]),
+    TagsModule
+  ],
+  controllers: [MeshNodesController],
+  providers: [MeshNodesService],
+  exports: [MeshNodesService],
+})
 export class MeshNodesModule {}
